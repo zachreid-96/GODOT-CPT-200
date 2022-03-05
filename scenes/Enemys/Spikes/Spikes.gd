7ef28cb2720e80c2a51b5540d11d12ae93ff0674
lukeahunt91 <91393041+lukeahunt91@users.noreply.github.com>
2022-03-05 10:52:32 -0600

    Updated Spikes.gd to enable full damage to player
    By making damage to player 100, this triggers player respawn animation and resets health to zero.

--- scenes/Enemys/Spikes/Spikes.gd
+++ scenes/Enemys/Spikes/Spikes.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
+var damage = .5
+var inArea = false
+var player:Player
 
 func _on_Spikes_body_entered(body: Node) -> void:
-	if body.get_name() == "Player":
-		body.respawn()
+		if body.get_class() == "Player":
+			player = body
+			player.hurt(100)
