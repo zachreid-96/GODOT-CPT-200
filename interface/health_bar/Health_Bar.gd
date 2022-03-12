e6ae72298eab1c852295517f09ed6c0624b9470a
lukeahunt91 <91393041+lukeahunt91@users.noreply.github.com>
2022-03-12 10:55:58 -0600

    Interface Addition and Script Updates
    Updated Player Node Name and Interface Scripts
    Added HUD to the Level_2-NT Scene

--- interface/health_bar/Health_Bar.gd
+++ interface/health_bar/Health_Bar.gd
@@ -9,5 +9,5 @@ func initialize():
 func _on_Interface_health_updated():
 	emit_signal("maximum_changed")
 
-func _on_Player2_health_changed():
+func _on_Player_health_changed():
 	emit_signal("maximum_changed")
